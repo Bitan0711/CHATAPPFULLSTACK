5f63fb787349a7f806a06ac22761f32037a18aed
Bitan0711 <bitan030903@gmail.com>
2026-07-09 16:27:07 +0530

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+//const express = require('express');
+import express from 'express';
+import "dotenv/config"
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
