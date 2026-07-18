1e0144a71c1bb9c67993c57a788998a0073be468
Bitan0711 <bitan030903@gmail.com>
2026-07-18 10:57:15 +0530

    Fix typos in index.js: correct 'cros' to 'cors' and 'prigin' to 'origin' in CORS middleware configuration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 //const express = require('express');
 import express from 'express';
 import "dotenv/config"
-import cros from "cors";
+import cors from "cors";
 
 import { clerkMiddleware } from "@clerk/express" ;
 
@@ -16,7 +16,7 @@ const port = process.env.PORT ;
 const FRONTEND_URL = process.env.FRONTEND_URL ;
 
 app.use(express.json);
-app.use(cors({prigin:FRONTEND_URL , credentials:true}));
+app.use(cors({origin:FRONTEND_URL , credentials:true}));
 app.use(clerkMiddleware());
 
 app.get("/health", (req, res) => {
